24a5472fa20d1152a6042716b63e090e5b9502e7
Mihir <aminvasudev6@gmail.com>
2023-08-21 01:37:14 +0530

    AuthSystem1.0.0

--- lib/screens/user_information_screen.dart
+++ lib/screens/user_information_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
 
 
@@ -12,8 +12,33 @@ class UserInformationScreen extends StatefulWidget {
 }
 
 class _UserInformationScreenState extends State<UserInformationScreen> {
+
+  final nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+          child: Center(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                ),
+                Text("Enter Your Name", style: TextStyle(fontSize: 24,fontWeight: FontWeight.w500, color: Colors.black),),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
