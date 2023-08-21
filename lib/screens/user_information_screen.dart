028ea0eb8c3cb800b22807b61ae4ac6b3c6aba04
Mihir <aminvasudev6@gmail.com>
2023-08-21 19:38:40 +0530

    InfoScreen

--- lib/screens/user_information_screen.dart
+++ lib/screens/user_information_screen.dart
@@ -1,6 +1,9 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+
+import 'package:gap/gap.dart';
 
 
 
@@ -13,6 +16,7 @@ class UserInformationScreen extends StatefulWidget {
 
 class _UserInformationScreenState extends State<UserInformationScreen> {
 
+  File? image;
   final nameController = TextEditingController();
 
   @override
@@ -32,10 +36,42 @@ class _UserInformationScreenState extends State<UserInformationScreen> {
               children: [
                 InkWell(
                   onTap: () {},
+                  child: image == null ? const CircleAvatar(
+                    radius: 50,
+                    child: Icon(FluentSystemIcons.ic_fluent_person_accounts_filled, size: 30,),
+                  ): CircleAvatar(
+                    backgroundImage: FileImage(image!),
+                    radius: 50,
+                  ),
                 ),
+                const Gap(25),
                 Text("Enter Your Name", style: TextStyle(fontSize: 24,fontWeight: FontWeight.w500, color: Colors.black),),
+                Container(
+                  child: Column(
+                    children: [
+                      textField(hintText: "Aditi M", icon: Icons.account_circle, inputType: TextInputType.text, maxLines: 1, controller: nameController)
+                    ],
+                  ),
+                ),
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget textField({required String hintText, required IconData icon, required TextInputType inputType, required int maxLines, required TextEditingController controller}){
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: inputType,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          prefixIcon: Container(
+            
           ),
         ),
       ),
