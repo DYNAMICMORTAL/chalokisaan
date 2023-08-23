c1ffdc7273cdbd268ad8aad2d73689e286eec524
Mihir <aminvasudev6@gmail.com>
2023-08-23 23:24:48 +0530

    UIFix2.4.0

--- lib/screens/packages/package_view.dart
+++ lib/screens/packages/package_view.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../utils/style.dart';
+
 class ExPackages extends StatelessWidget {
   const ExPackages({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+        backgroundColor: Color(0xFFF8D7AFF),
+        title: Center(
+        child: Text(
+        "Nashik Vineyard Experience Project", style: TextStyle(overflow: TextOverflow.ellipsis, color: Colors.white, fontWeight: FontWeight.w700),
+
+    ),
+    ),),
       body: SafeArea(
-        child: Column(),
+        child: Column(
+
+        ),
       ),
     );
   }
